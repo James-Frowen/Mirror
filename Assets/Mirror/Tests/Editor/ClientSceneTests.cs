5c36edb741b5062e4918255b910ca8bd7c239af3
James Frowen <jamesfrowendev@gmail.com>
2020-04-01 02:05:39 +0100

    tests

---
+++ Assets/Mirror/Tests/Editor/ClientSceneTests.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+
+namespace Mirror.Tests
+{
+    public class ClientSceneTests
+    {
+        [Test] [Ignore("Not Implemented")] public void AppliesPayloadCorrectly() { Assert.Fail(); }
+        [Test] [Ignore("Not Implemented")] public void SpawnsPrefabIfOneExists() { Assert.Fail(); }
+        [Test] [Ignore("Not Implemented")] public void SpawnsObjectFromSpawnHandler() { Assert.Fail(); }
+        [Test] [Ignore("Not Implemented")] public void AppliesPayloadToObjectInSpawnAsync() { Assert.Fail(); }
+        [Test] [Ignore("Not Implemented")] public void SpawnsSceneObject() { Assert.Fail(); }
+
+
+        [Test] [Ignore("Not Implemented")] public void RegisterPrefab() { Assert.Fail(); }
+        [Test] [Ignore("Not Implemented")] public void RegisterPrefabSetsAssetId() { Assert.Fail(); }
+        [Test] [Ignore("Not Implemented")] public void RegisterSpawnDelegate() { Assert.Fail(); }
+        [Test] [Ignore("Not Implemented")] public void RegisterSpawnHandlerDelegate() { Assert.Fail(); }
+        [Test] [Ignore("Not Implemented")] public void RegisterSpawnAsyncHandlerDelegate() { Assert.Fail(); }
+
+        [Test] [Ignore("Not Implemented")] public void RegisterSpawnHandlerGivesErrorIfWrongHandlerAlreadyExists() { Assert.Fail(); }
+        [Test] [Ignore("Not Implemented")] public void RegisterSpawnAsyncHandlerGivesErrorIfWrongHandlerAlreadyExists() { Assert.Fail(); }
+
+        [Test] [Ignore("Not Implemented")] public void UnregisterPrefabRemovesAllHandlers() { Assert.Fail(); }
+
+
+
+        [Test] [Ignore("Not Implemented")] public void GetPrefabReturnsTrueAndPrefab() { Assert.Fail(); }
+        [Test] [Ignore("Not Implemented")] public void GetPrefabReturnsFalseForNoPrefab() { Assert.Fail(); }
+
+
+
+    }
+}
